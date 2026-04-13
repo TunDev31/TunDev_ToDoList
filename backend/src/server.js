2dd8f9492579aaaca61c367b3721f6687d56fd72
TunDev31 <nguyenvqtrung31@gmail.com>
2026-04-13 17:41:46 +0700

    sap deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,14 +4,25 @@ import { connectDB } from './config/db.js';
 import dns from 'dns';
 import dotenv from 'dotenv'; 
 import cors from 'cors';
+import path from 'path';
 dns.setServers(['8.8.8.8', '8.8.4.4']);
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
-
+const __dirname = path.resolve();
 app.use(express.json());
-app.use(cors({origin:["http://localhost:5173"]}));
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({ origin: "http://localhost:5173" }));
+}
 app.use('/api/tasks', tasksRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 connectDB().then (()=> {
     app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
